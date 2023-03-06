422b80bf7dbacb4a3267e398d1f0094c5730670c
Tristan <tristan.adwitya@student.uns.ac.id>
2023-03-06 10:15:21 +0700

    Posi Pertemuan pertama

---
+++ Tristan.py
@@ -0,0 +1,16 @@
+#String
+nama_depan = "Tristan"
+nama_belakang = "Adwitya"
+#Integer
+angka = 10
+#Float
+desimal = 7.5
+#boolean
+boolean = True
+#list
+Kelas_D = ("Tristan", "kelas D", "dll")
+#dictionary
+dictionary = {"nama": "tristan", "kelas": "kelas D"}
+print("Nama saya adalah Tristan")
+print(nama_depan)
+print(f"nama saya adalah {nama_depan} {nama_belakang} dari kelas {dictionary['kelas']}")
