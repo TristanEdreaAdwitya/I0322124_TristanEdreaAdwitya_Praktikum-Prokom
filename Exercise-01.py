1d2deaaedfbda8b1ea53916f90a25cca60d82606
Tristan <tristan.adwitya@student.uns.ac.id>
2023-03-12 18:45:34 +0700

    Exercise-01

---
+++ Exercise-01.py
@@ -0,0 +1,12 @@
+Nama = "Tristan Edrea Adwitya"
+nim = "I0322124"
+Tempat_Tinggal = "Bekasi"
+Visualisasi = "Rumahku berada di ujung gang yang terdapat bundaran. Memiliki pagar hitam yang cukup panjang, serta pohon mangga yang menjulang tinggi. Rumahku tergolong cukup asri karena kakakku yang memiliki hobi menanam dan merawat tanaman. Yang aku rindui dari rumahku adalah suasana lingkunganya, dimana ramai dan penuh dengan anak-anak kecil bermain."
+Harapan = "Harapan saya setelah menjadi lulusan teknik industri uns adalah menjadi seorang data science. Data science berguna untuk mencari problem solving suatu perusahaan dengan data sebagai acuan agar suatu perusahaan bisa memutuskan dengan tepat."
+print('========================')
+print("Nama :", Nama)
+print("NIM :",nim)
+print("Tempat Tinggal :",Tempat_Tinggal)
+print("Visualisasi :",Visualisasi)
+print("Harapan :", Harapan)
+print('========================')
